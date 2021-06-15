cf0dbebc671fc7fcfa7e10bddf8b5c660955dbb3
Naeem Tahir <naeem.tahir@yahoo.com>
2021-06-15 17:24:07 -0400

    Add .wmv support

--- VideoManagement/2mp4.sh
+++ VideoManagement/2mp4.sh
@@ -7,8 +7,8 @@ NO_COLOR='\e[0m'
 
 OLD_IFS="$IFS"
 IFS=$'\n'
-for file in `find . -maxdepth 1 -type f \( -iname "*.mpg" -o -iname "*.3gp" \)`; do
-  newname=$(echo ${file} | sed 's/\.mpg/\.mp4/I; s/\.3gp/\.mp4/I')
+for file in `find . -maxdepth 1 -type f \( -iname "*.mpg" -o -iname "*.3gp" -o -iname "*.wmv" \)`; do
+  newname=$(echo ${file} | sed 's/\.mpg/\.mp4/I; s/\.3gp/\.mp4/I; s/\.wmv/\.mp4/I')
 
   echo -e "${GREEN}Converting \"${file}\" => \"${newname}\"${NO_COLOR}"
 	ffmpeg -i "${file}" "${newname}"
