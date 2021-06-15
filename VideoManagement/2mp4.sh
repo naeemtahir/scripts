4948a4cd9c71013d8d6c4994b8404a125285b89d
Naeem Tahir <naeem.tahir@yahoo.com>
2021-06-15 15:14:46 -0400

    Add script to convert legacy formats to MP4

---
+++ VideoManagement/2mp4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Convert MPG/3GP videos to MP4 format
+
+GREEN='\e[32m'
+NO_COLOR='\e[0m'
+
+OLD_IFS="$IFS"
+IFS=$'\n'
+for file in `find . -maxdepth 1 -type f \( -iname "*.mpg" -o -iname "*.3gp" \)`; do
+  newname=$(echo ${file} | sed 's/\.mpg/\.mp4/I; s/\.3gp/\.mp4/I')
+
+  echo -e "${GREEN}Converting \"${file}\" => \"${newname}\"${NO_COLOR}"
+	ffmpeg -i "${file}" "${newname}"
+done
+IFS="$OLD_IFS"
