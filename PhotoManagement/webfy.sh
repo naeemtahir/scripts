a257e0aa596f4d16321ff475dda5e52007f19bb8
Naeem Tahir <naeem.tahir@yahoo.com>
2023-05-22 00:34:21 -0400

    Updated script to skip renaming file when counter is 0

--- PhotoManagement/webfy.sh
+++ PhotoManagement/webfy.sh
@@ -5,29 +5,31 @@ re='^[0-9]+$'
 if [ "$#" -eq 2 ] && [[ $1 =~ $re ]] && [[ $2 =~ $re ]]; then
 	width=$1
 	counter=$2
-	prefix=`cat /dev/urandom | tr -dc 'A-Z' | fold -w 4 | head -n 1`
-	
+	prefix=$(cat /dev/urandom | tr -dc 'A-Z' | fold -w 4 | head -n 1)
+
 	OLD_IFS="$IFS"
 	IFS=$'\n'
-	for file in `find . -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | sort`; do
+	for file in $(find . -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | sort); do
 		fext="${file##*.}"
-  
+
 		mogrify -strip -auto-orient "$file"
-		
+
 		if [ "$width" -gt 0 ]; then
 			mogrify -resize $width -verbose "$file"
 		fi
-		
-		newname=`printf "%s_%06d" "$prefix" "$counter"`
-		
-		mv "$file" "$newname"."$fext"
-		
+
+		newname=$(printf "%s_%06d" "$prefix" "$counter")
+
+		if [ "$2" -gt 0 ]; then
+			mv "$file" "$newname"."$fext"
+		fi
+
 		counter=$((counter+1));
 	done
-	IFS="$OLD_IFS"	
+	IFS="$OLD_IFS"
 else
-    script_name=`basename "$0"`
+    script_name=$(basename "$0")
     echo "Strips EXIF data, shrinks to given width, and renames image files."
-    echo "  Usage: $script_name <width (0 for no resize)> <file_counter>"
+    echo "  Usage: $script_name <width (0 for no resize)> <file_counter (0 for no rename)>"
 	exit 1
 fi
