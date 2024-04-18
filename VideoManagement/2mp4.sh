078e2270d023b85d33a0a91c0543810a444d4fd6
Naeem Tahir <naeem.tahir@yahoo.com>
2024-04-18 17:33:06 -0400

    Add .m4v support

--- VideoManagement/2mp4.sh
+++ VideoManagement/2mp4.sh
@@ -7,8 +7,8 @@ NO_COLOR='\e[0m'
 
 OLD_IFS="$IFS"
 IFS=$'\n'
-for file in `find . -maxdepth 1 -type f \( -iname "*.mpg" -o -iname "*.3gp" -o -iname "*.wmv" -o -iname "*.avi" -o -iname "*.mov" \)`; do
-  newname=$(echo ${file} | sed 's/\.mpg/\.mp4/I; s/\.3gp/\.mp4/I; s/\.wmv/\.mp4/I; s/\.avi/\.mp4/I; s/\.mov/\.mp4/I')
+for file in `find . -maxdepth 1 -type f \( -iname "*.mpg" -o -iname "*.3gp" -o -iname "*.wmv" -o -iname "*.avi" -o -iname "*.mov"  -o -iname "*.m4v" \)`; do
+  newname=$(echo ${file} | sed 's/\.mpg/\.mp4/I; s/\.3gp/\.mp4/I; s/\.wmv/\.mp4/I; s/\.avi/\.mp4/I; s/\.mov/\.mp4/I; s/\.m4v/\.mp4/I')
 
   echo -e "${GREEN}Converting \"${file}\" => \"${newname}\"${NO_COLOR}"
 	ffmpeg -i "${file}" "${newname}"
