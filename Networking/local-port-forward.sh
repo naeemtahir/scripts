6de9fe10cd0bd99dc50915cf0df375233f77deaf
Naeem Tahir <naeem.tahir@yahoo.com>
2024-04-18 17:33:06 -0400

    Add port forwarding, proxying scripts

---
+++ Networking/local-port-forward.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$#" -eq 4 ]; then
+    REMOTE_USER=$1
+    REMOTE_HOST=$2
+    SERVICE_PORT=$3
+    LOCAL_PORT=$4
+
+    echo "Forwarding local port $LOCAL_PORT to $REMOTE_USER@$REMOTE_HOST. Access remote service on localhost:$LOCAL_PORT"
+    
+    ssh -L $LOCAL_PORT:localhost:$SERVICE_PORT $REMOTE_USER@$REMOTE_HOST -N
+else
+    script_name=`basename "$0"`
+    echo "Access remote service on local_port"
+    echo "  usage: $script_name <remote_user> <remote_host> <service_port> <local_port>"
+    
+    exit 1
+fi
