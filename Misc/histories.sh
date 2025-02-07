b69a870fae92b0de05b901825304443102407a40
Naeem Tahir <naeem.tahir@yahoo.com>
2025-02-07 11:10:26 -0500

    Add .sh suffix to sanitize_amp script. Disable sqlite history

--- Misc/histories.sh
+++ Misc/histories.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Disable all histories.
-
-# Get list of all dot files: 'ls -CAF -1 | grep -v /''
+# Disable histories.
 
 declare -a hist_files=(
    "$HOME/.bash_history"
    "$HOME/.zsh_history"
    "$HOME/.python_history"
+   "$HOME/.sqlite_history"
    "$HOME/.lesshst"
    "$HOME/.viminfo"
    "$HOME/.local/share/recently-used.xbel"
@@ -16,6 +15,8 @@ declare -a hist_files=(
    
 for f in "${hist_files[@]}"
 do
-   rm $f && touch $f && chmod 400 $f
+   rm $f
+   touch $f && chmod 400 $f
    ls -alF $f
 done
+
