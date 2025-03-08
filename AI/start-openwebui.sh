d055165a3b93f3767ff510e7c7cd1b49cc134f50
Naeem Tahir <naeem.tahir@yahoo.com>
2025-03-07 23:59:17 -0500

    Set environment variable to use WebUI as regular search engine

--- AI/start-openwebui.sh
+++ AI/start-openwebui.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+# '-e WEBUI_URL="http://localhost:3000"' is needed only if you want to integrate OpenWebUI as Browser Search Engine, you can skip it otherwise. Reference 'https://docs.openwebui.com/tutorials/integrations/browser-search-engine/'.
 echo "Pulling and running OpenWebUI container from web..."
-docker container run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+docker container run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always -e WEBUI_URL="http://localhost:3000" ghcr.io/open-webui/open-webui:main
 
 if [ $? -eq 125 ]; then
    echo "OpenWebUI container already exists, starting existing container..."
