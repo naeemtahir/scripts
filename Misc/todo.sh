00ebe04c27d175220ccf667eb8188d3bd58de635
Naeem Tahir <naeem.tahir@yahoo.com>
2025-10-15 22:56:08 -0400

    Restore todo.sh to pre-'Add support for completed tasks' version.

--- Misc/todo.sh
+++ Misc/todo.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-####################################################################################
-# Command-line Task Manager. Displays or edits task list file in following format: #
-#                                                                                  #
-# - [ ] ToDo Task 2                                                                #
-# - [ ] ToDo Task 3                                                                #
-# - [ ] ToDo Task 4                                                                #
-#                                                                                  #
-# ---                                                                              #
-#                                                                                  #
-# - [x] Completed Task 1                                                           #
-####################################################################################
-
 # https://misc.flogisoft.com/bash/tip_colors_and_formatting
 RED='\e[31m'
 GREEN='\e[32m'
@@ -21,100 +9,59 @@ LIGHT_CYAN='\e[96m'
 WHITE='\e[97m'
 BLINK='\e[5m'
 
-TASK_FILE="$TODO"
 
-display_header() {
+display_todos() {
     printf "${GREEN} _____ ___  ____   ___       ${NO_COLOR}\n"
     printf "${GREEN}|_   _/ _ \|  _ \ / _ \ ___  ${NO_COLOR}\n"
     printf "${GREEN}  | || | | | | | | | | / __| ${NO_COLOR}\n"
     printf "${GREEN}  | || |_| | |_| | |_| \__ \ ${NO_COLOR}\n"
     printf "${GREEN}  |_| \___/|____/ \___/|___/ ${NO_COLOR}\n"
     printf "\n"
-}
 
-display_footer() {
+    cat "$TODO"
+
     printf "\n"
     printf "${LIGHT_CYAN}Pass -e or -g to edit TODOs${NO_COLOR}\n"
 }
 
-# Function to display incomplete tasks
-display_incomplete_tasks() {
-    display_header
-    grep "^- \[ \]" "$TASK_FILE" || echo "No incomplete tasks found."
-    display_footer
-}
-
-# Function to process completed tasks
-process_completed_tasks() {
-    # Read file content into array
-    mapfile -t lines < "$TASK_FILE"
-
-    # Arrays for different sections
-    incomplete=()
-    completed=()
-    separator_found=false
-    separator="---"
-
-    # Process each line
-    for line in "${lines[@]}"; do
-        if [[ $line == $separator ]]; then
-            separator_found=true
-            continue
-        fi
-
-        if [[ $line =~ ^-\ \[x\] ]]; then
-            completed+=("$line")
-        # elif [[ $line =~ ^-\ \[ \] ]] && ! $separator_found; then
-        elif [[ $line =~ ^-\ \[\ \] ]] && ! $separator_found; then
-            incomplete+=("$line")
-        fi
-    done
-
-    # Write new content back to file
-    {
-        for line in "${incomplete[@]}"; do
-            echo "$line"
-        done
-        echo ""
-        echo "$separator"
-        echo ""
-        for line in "${completed[@]}"; do
-            echo "$line"
-        done
-    } > "$TASK_FILE"
-}
-
-if [ ! -f "$TASK_FILE" ]; then
-    printf "${RED}File not found: ${TASK_FILE}${NO_COLOR}\n"
+if [ -z "$TODO" ]; then
+    printf "${RED}Environment variable TODO not defined.${NO_COLOR}\n"
     exit 1
 fi
 
-# Check for argument
-if [ "$1" == "-e" ] || [ "$1" == "-g" ]; then
-    # Open file in editor
-    if [[ "$OSTYPE" == "linux"* ]] && ([[ $(uname -a) =~ .*Microsoft.* ]] || [[ $(uname -a) =~ .*WSL2.* ]]); then
-        vi "$TASK_FILE"
-    elif [[ "$OSTYPE" == "linux"* ]]; then
-        if [ "$1" == "-g" ]; then
-            xdg-open "$TASK_FILE" &
-        else
-            vi "$TASK_FILE"
-        fi
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        if [ "$1" == "-g" ]; then
-            open "$TASK_FILE" &
-        else
-            vi "$TASK_FILE"
-        fi
-    else
-        vi "$TASK_FILE"
-    fi
+if [ ! -f "$TODO" ]; then
+    printf "${RED}File not found: ${TODO}${NO_COLOR}\n"
+    exit 1
+fi
 
-    # Process completed tasks after editing
-    process_completed_tasks
-    # Display incomplete tasks
-    display_incomplete_tasks
+if [ "$#" -ge 1 ]; then
+    if [ "$1" == "-e" ] || [ "$1" == "-g" ]; then
+        if [[ "$OSTYPE" == "linux"* ]] && ([[ $(uname -a) =~ .*Microsoft.* ]] || [[ $(uname -a) =~ .*WSL2.* ]]); then
+            vi "$TODO"
+        elif [[ "$OSTYPE" == "linux"* ]]; then
+            if [ "$1" == "-g" ]; then
+                xdg-open "$TODO" &
+            else
+                vi "$TODO"
+            fi
+        elif [[ "$OSTYPE" == "darwin"* ]]; then
+            if [ "$1" == "-g" ]; then
+                open "$TODO" &
+            else
+                vi "$TODO"
+            fi
+        else
+            vi "$TODO"
+        fi
+
+        display_todos
+    else
+        script_name=$(basename "$0")
+        echo "View or edit (use -e) your TODOs file (i.e., $TODO)."
+        echo "  Usage: $script_name [-e] [-g]"
+        echo "      -e Edit TODOs in vi"
+        echo "      -g Edit TODOs in default Graphics editor"
+    fi
 else
-    # Display incomplete tasks
-    display_incomplete_tasks
+    display_todos
 fi
